415fc5b34842d8bdab0d7c1265e4c3b2e58c0206
Victouch <vicodion11@gmail.com>
2023-01-27 13:47:34 +0100

    first commit

---
+++ home/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Products(models.Model):
+    product_name = models.CharField(max_length=500)
+    product_image = models.ImageField(upload_to='image')
+
+
